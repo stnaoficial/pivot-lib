487830f9b7b14ed607c7f3ba7d919fdf9506961f
Pedro Henrique Diogo de Santana <pedro.santana@facedigital.com.br>
2022-10-18 18:38:19 -0300

    Core update

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,152 +1,100 @@
-// import { cloneDeep, iterate, overwrite, pascalCaseToKebabCase, script } from "../utils/index";
+import { overwrite } from "../utils/index";
 
-// export namespace PivotCore
-// {
-//     class Component
-//     {
-//         public static UseTemplate(template: any, templateElement: HTMLElement)
-//         {
-//             customElements.define("pivot-component", class extends HTMLElement {
-//                 public constructor() 
-//                 { 
-//                     super();
-//                 }
+/**
+ * Global Declarations
+ */
+declare global {
+    interface Window { PivotSession: any; }
+}
 
-//                 public connectedCallback(): void
-//                 {
-//                     if (
-//                         this.hasAttribute("name")
-//                         && this.getAttribute("name") === templateElement.getAttribute("for")
-//                     ) {
-//                         this.innerHTML = templateElement.innerHTML;
-//                     }
-
-//                     var target: any = {};
-        
-//                     target.data    = overwrite(template.dataWillBeDefined(cloneDeep(template.data())), this.dataset);        
-//                     target.element = this;
-
-//                     if (
-//                         this.hasAttribute("script")
-//                     ) {
-//                         target = script(this.getAttribute("script"), target);
-//                     }
-
-//                     template.whenDefined.apply(template, [target.element, target.data]);
-//                 }
-                
-//                 public disconnectedCallback(): void
-//                 {
-//                     /** Do something */
-//                 }
-                
-//                 public adoptedCallback(): void
-//                 {
-//                     /** Do something */
-//                 }
-                
-//                 public attributeChangedCallback(name: string, oldValue: string, newValue: string): void
-//                 {
-//                     /** Do something */
-//                 }
-//             });
-//         }
-//     }
-
-//     export class Template
-//     {
-//         constructor()
-//         {
-//             const template: any = this.constructor.prototype || new.target;
-
-//             customElements.define("pivot-template", class extends HTMLElement {
-//                 public constructor() 
-//                 { 
-//                     super();
-//                 }
-
-//                 public connectedCallback(): void
-//                 {
-//                     if (
-//                         this.hasAttribute("use")
-//                     ) {
-//                         Component.UseTemplate(template, this);
-//                     }
-//                 }
-//             });
-//         }
-
-//         public data(): any
-//         {
-//             return {};
-//         }
-        
-//         public dataWillBeDefined<T extends any>(data: T): T
-//         {
-//             return data;
-//         }
-        
-//         public whenDefined(element?: HTMLElement, data?: any): any
-//         {
-//             /** Do something */
-//         }
-
-//         public setOutput(element: HTMLElement, outputName: string, outputValue: string): void
-//         {
-//             const outputElements = element.querySelectorAll(`[output-${outputName}]`);
-
-//             Object.values(outputElements).map(outputElement => {
-//                 outputElement.innerHTML = outputValue;
-//             })
-//         }
-//     }
-// }
-
-
-new class Pivot
+export namespace PivotCore
 {
-    occur: Array<object>;
-
-    constructor()
+    export class Controller
     {
-        const _occur: Array<object> = [];
-
-        customElements.define(`pivot-new`, class extends HTMLElement {
-            public constructor() 
-            { 
-                super();
-            }
-
-            public connectedCallback(): void
-            {
-                
-                if (this.hasAttribute("extends")) {
-                    const _extends: string = this.getAttribute("extends") ?? "";
-
-                    fetch(
-                        _extends
-                    )
-                    .then(response => {
-                        return response.text;
-                    })
-                    .then(text => {
-                        const parser = new DOMParser();
-
-                        console.log(text())
-
-                        this.append(parser.parseFromString(text, "text/html"));
-                    })
-                    .catch(() => {
-                        console.error("Cannot extend this pivot");
-                    })
-                }
-
-                _occur.push(this);
-            }
-        });
-
-        this.occur = _occur;
-
-        console.log(this)
+        constructor()
+        {
+            PivotSession.UseController(new.target);
+        }
+        data(): object
+        {
+            return {}
+        }
+        whenDefined(self: HTMLElement, data: object): void
+        {
+            /** Do something */
+        }
     }
+
+    class PivotSessionHandlers
+    {
+        static UseElement(text: string, dataset: DOMStringMap): string
+        {
+            return text.replace(/\{\{(.*?)\}\}/g, function(match: string, value: string): string {
+                return dataset[value.trim()] ?? value;
+            });
+        }
+
+        static UseController(ctrl: any)
+        {
+            window.PivotSession.inUse.map((self: HTMLElement) => {
+                if (ctrl.name === self.getAttribute("use")) {
+                    ctrl.prototype.whenDefined(self, overwrite(ctrl.prototype.data(), self.dataset));
+                }
+            });
+        }
+    }
+
+    class PivotSession extends PivotSessionHandlers
+    {
+        occurs?: Array<object>;
+        inUse?:  Array<object>;
+    
+        constructor()
+        {
+            super();
+
+            const _occurs: Array<object> = [];
+            const _inUse: Array<object> = [];
+    
+            customElements.define(`pvt-new`, class extends HTMLElement {
+                public constructor() 
+                { 
+                    super();
+                }
+    
+                /** Populate */
+                public connectedCallback(): void
+                {
+                    if (this.hasAttribute("use")) {
+                        /** Do something */
+                        _inUse.push(this);
+                    }
+                    
+                    if (this.hasAttribute("extends")) {
+                        const _extends: string = this.getAttribute("extends") ?? "";
+    
+                        fetch(
+                            _extends
+                        )
+                        .then(response => {
+                            return response.text();
+                        })
+                        .then(textElement => {
+                            this.innerHTML = PivotSession.UseElement(textElement, this.dataset);
+                        })
+                        .catch(error => {
+                            console.error(error);
+                        })
+                    }
+    
+                    _occurs.push(this);
+                }
+            });
+    
+            this.occurs = _occurs;
+            this.inUse = _inUse;
+        }
+    }
+
+    window.PivotSession = window.PivotSession || new PivotSession;
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,50 +1,32 @@
-// import { PivotCore } from "../core/pivot-core";
-// import { isNull, onViewport, setCounter } from "../utils/index";
+import { PivotCore } from "../core/index";
 
-// new class Writer extends PivotCore.Template
-// {
-//     public data(): any
-//     {
-//         return {
-//             message: "Lorem Ipsum",
-//             interval: 50,
-//             timeout: 0,
-//             onloadclass: "",
-//             statement: ""
-//         }
-//     }
+new class Messager extends PivotCore.Controller
+{
+    constructor()
+    {
+        super();
+    }
+    data(): object
+    {
+        return {
+            title: "dadsdas",
+            message: "aeee"
+        }
+    }
+    whenDefined(self: HTMLElement, data: object)
+    {
+        console.log(self, data);
+    }
+}
 
-//     public whenDefined(element: HTMLElement, data: any): void
-//     {        
-//         const output = element.querySelector("[data-message]") || new HTMLElement;
-
-//         const outputInnerHTMLBackup = output.innerHTML;
-
-//         onViewport(element).then(() => {
-//             if (!isNull(parseInt(data.timeout))) {
-//                 setTimeout(() => { startTyping(); }, parseInt(data.timeout));
-//             } else {
-//                 startTyping();
-//             }
-//         })
-        
-//         function startTyping(): void {
-//             if (!isNull(data.onloadclass)) {
-//                 element.classList.add(data.onloadclass);
-//             }
-
-//             output.innerHTML = "";
-
-//             setCounter((newValue) => {
-//                 if (data.message[newValue as keyof (typeof data.message)] !== undefined) {
-//                     output.innerHTML += data.message[newValue as keyof (typeof data.message)];
-//                 }
-
-//             }, 0, data.message.length, parseInt(data.interval.toString())).then(() => {
-//                 output.innerHTML += outputInnerHTMLBackup;
-//             });
-//         }
-
-//         return data;
-//     }
-// }
+new class XML extends PivotCore.Controller
+{
+    constructor()
+    {
+        super();
+    }
+    whenDefined(self: HTMLElement, data: object)
+    {
+        console.log(self, data);
+    }
+}
